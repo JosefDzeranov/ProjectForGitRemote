5266ace49b5c58c47fb2b189f053f088564f9a6c
Svetlana <lanako0507@gmail.com>
2023-07-27 22:07:30 +0300

    Hello World!!

--- ProjectForGit/Program.cs
+++ ProjectForGit/Program.cs
@@ -6,7 +6,7 @@ namespace ProjectForGit
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!!");
 
             // сделали снова изменения не в той ветке
 
