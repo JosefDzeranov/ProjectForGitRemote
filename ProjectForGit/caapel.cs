a4e145d7e1ae42ff9b113198f46bf6f6449a8764
Станислав Саитов <caapel@mail.ru>
2023-12-29 10:39:39 +0300

    изменения в новой от main ветке

---
+++ ProjectForGit/caapel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectForGit
+{
+    internal class caapel
+    {
+        //добавляю изменения в рамках практического занятия "Технология GIT"
+        //для получения внешнего Rewiev для Fork-репозитория
+    }
+}
